62bbddf4196aed8ca7a22a2104f013bf1a4164de
LEGION <rc.vasquez27@gmaill.com>
2025-02-26 13:00:26 -0600

    Estructura maven

---
+++ testStoreScrennPlay/src/main/java/testStoreScreenPlay/RicardoV/App.java
@@ -0,0 +1,13 @@
+package testStoreScreenPlay.RicardoV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
